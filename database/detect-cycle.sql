a0f454f00d34c37e420b918a091ae292f2126bf5
Gregory Higginson <GHigginson@appneta.com>
2015-12-02 20:59:03 -0800

    Updated to work with new schema

--- database/detect-cycle.sql
+++ database/detect-cycle.sql
@@ -1,73 +1,44 @@
-﻿/*
-create table nodes (
-  n integer not null
-);
-delete from nodes;
-insert into nodes values (1);
-insert into nodes values (2);
-insert into nodes values (3);
-insert into nodes values (4);
-create table edges (
-  s integer not null,
-  c integer not null,
-  d integer not null
-);
-delete from edges;
-insert into edges (s,c,d) values (1,1,2);
-insert into edges (s,c,d) values (1,5,3);
-insert into edges (s,c,d) values (2,1,3);
---insert into edges (s,c,d) values (2,1,1);
---insert into edges (s,c,d) values (4,1,4);
-
-
-insert into edges (s,c,d) values (1,1,1);
-insert into edges (s,c,d) values (1,1,2);
-insert into edges (s,c,d) values (2,5,3);
-insert into edges (s,c,d) values (3,1,2);
-insert into edges (s,c,d) values (2,1,4);
-*/
-
-
+﻿
 /* Uses dijkstra's algorithm, but with no root node, instead initializes all nodes
  * with no inbound edges to zero. Terminates any time an edge is encountered that
  * terminates in the set already spanned.
  */
-create or replace function detect_cycle_detect_take_2()
+create or replace function detect_cycle_detect_take_2(graphId integer)
 returns void AS $$
 declare
   vertex record;
-  edge record;
+  path record;
   newDistance integer;
 begin
   create temp table vertices (
-    node integer,
-    distance integer,
+    id integer,
+    distance real,
     previous integer,
     spanned boolean
   ) on commit drop;
-  insert into vertices select n, 99999999, null, false from nodes;
-  update vertices set distance = 0 where node not in (select d from edges);
+  insert into vertices select node.id, 99999999, null, false from node where graph_id = graphId;
+  update vertices set distance = 0.0 where id not in (select target_node_id from edge inner join node on edge.source_node_id = node.id and graph_id=graphId);
 
   loop
     select * into vertex from vertices where spanned=false order by distance asc limit 1;
     if (vertex is null) then
        exit;
     end if;
-    update vertices set spanned=true where node = vertex.node;
-    for edge in
-      select * from edges inner join vertices on d = vertices.node
-      where s=vertex.node
+    update vertices set spanned=true where id = vertex.id;
+    for path in
+      select * from vertices inner join edge on edge.target_node_id = vertices.id
+      where edge.source_node_id=vertex.id
     loop
-      if (edge.s = edge.d) then
-        raise Exception 'Cycle detected [%..%]', edge.s, edge.d;
+      if (path.source_node_id = path.target_node_id) then
+        raise Exception 'Cycle detected [%..%]', path.source_node_id, path.target_node_id;
       end if;
-      if exists (select * from vertices where node = edge.d and spanned=true) then
-        raise Exception 'Cycle detected [%..%..%]', edge.d, edge.s, edge.d;
+      if exists (select * from vertices where id = path.target_node_id and spanned=true) then
+        raise Exception 'Cycle detected [%..%..%]', path.target_node_id, path.source_node_id, path.target_node_id;
       end if;
-      newDistance := vertex.distance + edge.c;
-      if (newDistance < edge.distance) then
-	update vertices set distance = newDistance, previous=vertex.node
-	where node=edge.node;
+      newDistance := vertex.distance + path.cost;
+      if (newDistance < path.distance) then
+	update vertices set distance = newDistance, previous=vertex.id
+	where id=path.target_node_id;
       end if;
     end loop;
   end loop;
@@ -75,139 +46,108 @@ begin
 end
 $$ language plpgsql;
 
-select detect_cycle_detect_take_2();
-
-
-/*
--- naive solution using dijkstra
--- would need to call once on every potential source node....
-create or replace function dijkstra_cycle_detect(src integer)
-returns table (id integer, dist integer, prev integer) AS $$
+create or replace function dijkstra(graphId integer, rootNodeId integer)
+returns table (source_node varchar(8), dest_node varchar(8)) AS $$
 declare
   vertex record;
-  edge record;
-  newDistance integer;
+  path record;
+  newDistance real;
+  maxDistance real;
 begin
+  maxDistance := 1e20;
   create temp table vertices (
-    node integer,
-    distance integer,
+    id integer,
+    distance real,
     previous integer,
     spanned boolean
   ) on commit drop;
-  insert into vertices select n, 99999999, null, false from nodes;
-  update vertices set distance = 0 where node = src;
+  insert into vertices select node.id, maxDistance, null, false from node where graph_id = graphId;
+  update vertices set distance = 0.0 where id=rootNodeId;
 
   loop
     select * into vertex from vertices where spanned=false order by distance asc limit 1;
     if (vertex is null) then
        exit;
     end if;
-    update vertices set spanned=true where node = vertex.node;
-    for edge in
-      select * from edges inner join vertices on d = vertices.node
-      where s=vertex.node
+    update vertices set spanned=true where id = vertex.id;
+    for path in
+      select * from vertices inner join edge on edge.target_node_id = vertices.id
+      where edge.source_node_id=vertex.id
     loop
-      if (edge.s = edge.d) then
-        raise Exception 'Cycle detected [%..%]', edge.s, edge.d;
-      end if;
-      if exists (select * from vertices where node = edge.d and spanned=true) then
-        raise Exception 'Cycle detected [%..%..%]', edge.d, edge.s, edge.d;
-      end if;
-      newDistance := vertex.distance + edge.c;
-      if (newDistance < edge.distance) then
-	update vertices set distance = newDistance, previous=vertex.node
-	where node=edge.node;
+      newDistance := vertex.distance + path.cost;
+      if (newDistance < path.distance) then
+	update vertices set distance = newDistance, previous=vertex.id
+	where id=path.target_node_id;
       end if;
     end loop;
   end loop;
-  return query select node, distance, previous from vertices;
+  delete from vertices where distance=maxDistance;
+  return query select a.cid, b.cid from vertices
+     inner join node a on vertices.id = a.id
+     left outer join node b on vertices.previous = b.id;
 end
 $$ language plpgsql;
 
-select dijkstra_cycle_detect(1);
+
+/*************
+* Test Cases *
+*************/
+
+/* 
+-- NO CYCLE
+delete from edge;
+delete from node;
+delete from graph;
+insert into graph values (1, 'g1', 'g1');
+insert into node values (1,1,'n1','n1');
+insert into node values (2,1,'n2','n2');
+insert into node values (3,1,'n3','n3');
+insert into node values (4,1,'n4','n4');
+insert into edge values (1,'e1',1,2,1);
+insert into edge values (2,'e2',1,3,5);
+insert into edge values (3,'e3',2,3,1);
+
+select detect_cycle_detect_take_2(1);  -- No Error
+select dijkstra(1,1); -- n3->n2->n1
+select dijkstra(1,4); -- n4
+select dijkstra(2,1); -- nothing (no such graph)
 */
 
 /*
--- works, but is excessive?
-create or replace function dijkstra(src integer)
-returns table (id integer, dist integer, prev integer) AS $$
-declare
-  nextNode integer;
-begin
-  create temp table vertices (
-    node integer,
-    distance integer,
-    previous integer,
-    spanned boolean
-  ) on commit drop;
-  insert into vertices select n, 99999999, null, false from nodes;
-  update vertices set distance = 0 where node = src;
+-- SIMPLE LOOP
+delete from edge;
+delete from node;
+delete from graph;
+insert into graph values (1, 'g1', 'g1');
+insert into node values (1,1,'n1','n1');
+insert into edge values (1,'e1',1,1,1);
 
-  loop
-    select node into nextNode from vertices where spanned=false order by distance asc limit 1;
-    if (nextNode is null) then
-       exit;
-    end if;
-    perform dijkstra_process_vertex(nextNode); -- TODO fold the method back in
-  end loop;
-
-  return query select node, distance, previous from vertices;
-end
-$$ language plpgsql;
-
--- works, does cycle detection
-create or replace function dijkstra_process_vertex(nodeId integer)
-returns void AS $$
-declare
-   vertex record;
-   edge record;
-   newDistance integer;
-begin
-    select * into vertex from vertices where node=nodeId;
-    update vertices set spanned=true where node = vertex.node;
-    for edge in
-      select * from edges inner join vertices on d = vertices.node
-      where s=vertex.node
-    loop
-      if (edge.s = edge.d) then
-        raise Exception 'Cycle detected [%..%]', edge.s, edge.d;
-      end if;
-      if exists (select * from vertices where node = edge.d and spanned=true) then
-        raise Exception 'Cycle detected [%..%..%]', edge.d, edge.s, edge.d;
-      end if;
-      newDistance := vertex.distance + edge.c;
-      if (newDistance < edge.distance) then
-	update vertices set distance = newDistance, previous=vertex.node
-	where node=edge.node;
-      end if;
-    end loop;
-end
-$$ language plpgsql;
-
--- works, no cycle detection
-create or replace function dijkstra_process_vertex(nodeId integer)
-returns void AS $$
-declare
-   vertex record;
-   edge record;
-   newDistance integer;
-begin
-    select * into vertex from vertices where node=nodeId;
-    update vertices set spanned=true where node = vertex.node;
-    for edge in
-      select * from edges inner join vertices on d = vertices.node
-      where s=vertex.node and d in (select node from vertices where spanned=false)
-    loop
-      newDistance := vertex.distance + edge.c;
-      if (newDistance < edge.distance) then
-	update vertices set distance = newDistance, previous=vertex.node
-	where node=edge.node;
-      end if;
-    end loop;
-end
-$$ language plpgsql;
-select dijkstra(1);
+select detect_cycle_detect_take_2(1);  -- Found cycle
 */
+
+/*
+-- COMPLEX LOOP
+delete from edge;
+delete from node;
+delete from graph;
+insert into graph values (1, 'g1', 'g1');
+insert into node values (1,1,'n1','n1');
+insert into node values (2,1,'n2','n2');
+insert into node values (3,1,'n3','n3');
+insert into node values (4,1,'n4','n4');
+insert into edge values (1,'e1',1,2,1);
+insert into edge values (2,'e2',1,3,5);
+insert into edge values (3,'e3',2,3,1);
+insert into edge values (4,'e4',2,1,1);
+insert into edge values (5,'e5',4,4,1);
+
+select detect_cycle_detect_take_2(1);  -- Found cycle
+select detect_cycle_detect_take_2(2);  -- Nothing
+*/
+
+
+
+
 
 
 
